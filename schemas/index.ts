0630eac24659117c879915a87bce0c7539534658
Guido La Rosa <guido.glarosa@gmail.com>
2023-12-19 15:48:41 -0300

    Updated studio

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,3 @@
-export const schemaTypes = []
+import offer from "./offer"
+
+export const schemaTypes = [offer]
---
+++ schemas/offer.ts
@@ -0,0 +1,17 @@
+export default {
+    name: 'offer',
+    type: 'document',
+      title: 'Offer',
+    fields: [
+      {
+        name: 'imageId',
+        type: 'number',
+        title: 'Image ID'
+      },
+      {
+        name: 'text',
+        type: 'string',
+        title: 'Offer Text'
+      }
+    ]
+  }
